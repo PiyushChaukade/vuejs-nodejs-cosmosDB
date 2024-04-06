e3ccc560e2a1b3341be8ecb5f803ff8926ff60d1
PiyushChaukade <93372962+PiyushChaukade@users.noreply.github.com>
2024-04-07 00:28:00 +0530

    made some changes for updating todo

--- Frontend/src/components/TodoCard.vue
+++ Frontend/src/components/TodoCard.vue
@@ -2,17 +2,42 @@
   <v-container class="pa-5 text-lg-left">
     <v-row>
       <v-col cols="4" v-for="(todos, index) in received" :key="index">
-
-        <v-card class="mx-auto mb-4" max-width="300" hover >
+        <v-card class="mx-auto mb-4" max-width="300" hover>
           <v-card-item text-lg-right>
-            <v-card-title text-lg-right 
+            <v-card-title text-lg-right
               >Title: {{ todos.TodoList }}</v-card-title
             >
           </v-card-item>
           <v-card-text>Description: {{ todos.description }}</v-card-text>
           <div class="d-flex">
             <v-sheet>
-              <UpdateTodo :todos="todos"  />
+              <UpdateTodo @click="updateTodohere(todos)" />
+              <v-dialog max-width="400" v-model="dialog">
+                <v-card title="Update Todo Items">
+                  <v-container class="pa-5">
+                    <v-sheet class="mx-auto" width="300">
+                      <v-form fast-fail @submit.prevent="submitForm">
+                        <v-text-field
+                          v-model="UpdatedTodoList"
+                          label="Todo List"
+                        ></v-text-field>
+                        <v-textarea
+                          label="Description"
+                          v-model="Updateddescription"
+                          name="input-7-1"
+                          variant="filled"
+                          auto-grow
+                        ></v-textarea>
+
+                        <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+                        <v-btn class="mt-2" @click="canceledForm" block
+                          >Cancel</v-btn
+                        >
+                      </v-form>
+                    </v-sheet>
+                  </v-container>
+                </v-card>
+              </v-dialog>
             </v-sheet>
             <v-sheet><DeleteTodo @click="deleteItem(todos.id)" /></v-sheet>
           </div>
@@ -31,9 +56,12 @@ export default {
   props: { deleteItem: Function },
   components: { DeleteTodo, UpdateTodo },
   data: () => ({
-    
+    dialog: false,
     data: null,
     loading: true,
+    UpdatedTodoList: "",
+    Updateddescription: "",
+    id: "",
   }),
   computed: {
     // ...mapState({
@@ -45,11 +73,34 @@ export default {
     this.fetchTodos();
   },
   methods: {
-    ...mapActions(["fetchTodos"]),
+    ...mapActions(["fetchTodos", "updateItem"]),
     async fetchData() {
       await this.fetchTodos();
     },
-   
+    updateTodohere(data) {
+      this.dialog = true;
+      this.UpdatedTodoList = data.TodoList;
+      this.Updateddescription = data.description;
+      this.id = data.id;
+    },
+    // ...mapActions(["fetchTodos", "updateItem"]),
+    async submitForm() {
+      this.dialog = false;
+      const UpdatedTodo = {
+        TodoList: this.UpdatedTodoList,
+        description: this.Updateddescription,
+        id: this.id,
+      };
+      console.log(UpdatedTodo, "UpdatedTodo");
+      await this.updateItem(UpdatedTodo);
+      await this.fetchTodos();
+    },
+    canceledForm() {
+      this.dialog = false;
+    },
+    //   created(){
+
+    // }
   },
 };
 </script>
--- Frontend/src/components/UpdateTodo.vue
+++ Frontend/src/components/UpdateTodo.vue
@@ -4,75 +4,52 @@
       color="blue-lighten-2"
       icon="mdi-pencil"
       variant="text"
-      @click="dialog = true"
+     
     >
     </v-btn>
-    <v-dialog max-width="400" v-model="dialog">
-        <v-card title="Update Todo Items">
-          <v-container class="pa-5">
-            <v-sheet class="mx-auto" width="300">
-              <v-form fast-fail @submit.prevent="submitForm">
-                <v-text-field
-                  v-model="UpdatedTodoList"
-                  label="Todo List"
-                ></v-text-field>
-                <v-textarea
-                  label="Description"
-                  v-model="Updateddescription"
-                  name="input-7-1"
-                  variant="filled"
-                  auto-grow
-                ></v-textarea>
-
-                <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                <v-btn class="mt-2" @click="canceledForm" block>Cancel</v-btn>
-              </v-form>
-            </v-sheet>
-          </v-container>
-        </v-card>
-      </v-dialog>
+   
   </v-container>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+// import { mapActions } from "vuex";
 export default {
   name: "UpdateTodo",
 
   props: {
-    todos:Object
+    // todos:Object
   },
 
   components: {},
 
   data: () => ({
-    dialog: false,
-    UpdatedTodoList: "",
-    Updateddescription: "",
-    id:''
+    // dialog: false,
+    // UpdatedTodoList: "",
+    // Updateddescription: "",
+    // id:''
   }),
 
   methods: {
-    ...mapActions(["fetchTodos", "updateItem"]),
-    async submitForm() {
-      this.dialog = false;
-      const UpdatedTodo = {
-        TodoList: this.UpdatedTodoList,
-        description: this.Updateddescription,
-        id:this.id,
-      };
-      console.log(UpdatedTodo,"UpdatedTodo")
-      await this.updateItem(UpdatedTodo);
-      await this.fetchTodos();
-    },
-    canceledForm() {
-      this.dialog = false;
-    },
+    // ...mapActions(["fetchTodos", "updateItem"]),
+    // async submitForm() {
+    //   this.dialog = false;
+    //   const UpdatedTodo = {
+    //     TodoList: this.UpdatedTodoList,
+    //     description: this.Updateddescription,
+    //     id:this.id,
+    //   };
+    //   console.log(UpdatedTodo,"UpdatedTodo")
+    //   await this.updateItem(UpdatedTodo);
+    //   await this.fetchTodos();
+    // },
+    // canceledForm() {
+    //   this.dialog = false;
+    // },
   },
   created(){
-    this.UpdatedTodoList=this.todos.TodoList;
-      this.Updateddescription=this.todos.description;
-      this.id=this.todos.id;
+    // this.UpdatedTodoList=this.todos.TodoList;
+    //   this.Updateddescription=this.todos.description;
+    //   this.id=this.todos.id;
   }
 };
 </script>
